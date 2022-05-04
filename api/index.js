721152790675eab68805e46c22f8ea3f00d53891
luis <luis.elias.david@gmail.com>
2022-05-04 18:11:38 -0300

    hola mundo

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const { conn } = require('./src/db.js');
 const response= require('./src/bdInfo/respuesta.json'); 
 const {Sneakers} = require('./src/db.js');
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
 
 Sneakers.bulkCreate(response.results)
 
