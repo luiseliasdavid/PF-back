b949eb9060d91ed23171df1908f7b20461380757
luis <luis.elias.david@gmail.com>
2022-05-05 20:22:05 -0300

    02

--- api/conflicts.js
+++ api/conflicts.js
@@ -3,9 +3,16 @@ fvfdvfvdfvfvdfvdfvfvd
 hiuhuihiuhiuhiu
 ggrdgrdgdrgdr
 grgrdgdrgrdg
+<<<<<<< Updated upstream
 
 probando pull request
 165165151651651651651615651651
 grgdrgdrgdrg
 grdgdrgdrg
 grdrgdrgdrgdr
+=======
+165165151651651651651615651651
+grgdrgdrgdrg
+grdgdrgdrg
+grdrgdrgdrgdr
+>>>>>>> Stashed changes
