2d1fd6d528a7609d2bc49e710424d59e4d555d9b
luiseliasdavid <luis.elias.david@gmail.com>
2022-05-06 13:40:56 -0300

    Update conflicts.js

--- api/conflicts.js
+++ api/conflicts.js
@@ -1,2 +1,4 @@
 'hola ulices'
 'creando rama developer'
+"git branch --set-upstream-to=origin/develop-brach ruta-pokemons"
+"este comando seria para seleccionar la rama remota desde donde hacer un pull"
