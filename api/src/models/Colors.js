7ea056e95f729532f069a450bcbdbc75a64c7cce
juan riquero <juanoriquero@cmail.comcd>
2022-05-10 09:05:09 -0300

    color con uuid

--- api/src/models/Colors.js
+++ api/src/models/Colors.js
@@ -5,6 +5,12 @@ module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('colors', 
   {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
