1b3eba9644ef30de1be8f5b9c8553886fe8a0bd2
Nicolás Cáceres Sala <ncsala@gmail.com>
2022-05-09 23:34:51 -0300

    Modelos creados detalle de orden y orden

--- api/src/models/Order.js
+++ api/src/models/Order.js
@@ -1,20 +1,37 @@
 const { DataTypes } = require('sequelize');
 
+// Se relaciona con id de usuario
 module.exports = (sequelize) => {
-  sequelize.define('order', {
-    paymentStatus: {
-      type: DataTypes.ENUM('On Cart', 'Creada', 'Procesando', 'Cancelada', 'Completa'),
-      defaultValue: 'On Cart',
-      allowNull: false
-    },
-    orderDate: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW,
-      allowNull: false
-    }
-  },
-    {
-      timestamps: false
-    }
-  );
+	sequelize.define(
+		'order',
+		{
+			// paymentStatus es para saber si el pago fue realizado o no
+			paymentStatus: {
+				type: DataTypes.ENUM(
+					'On Cart',
+					'Creada',
+					'Procesando',
+					'Cancelada',
+					'Completa'
+				),
+				defaultValue: 'On Cart',
+				allowNull: false,
+			},
+			// orderdate es para saber cuando se creo el pedido
+			orderDate: {
+				type: DataTypes.DATE,
+				defaultValue: DataTypes.NOW,
+				allowNull: false,
+			},
+			total: {
+				type: DataTypes.DECIMAL(10, 2),
+				defaultValue: 0,
+				allowNull: false,
+			},
+		},
+
+		{
+			timestamps: false,
+		}
+	);
 };
--- api/src/models/Order_Detail.js
+++ api/src/models/Order_Detail.js
@@ -1,20 +1,25 @@
 const { DataTypes } = require('sequelize');
 
+
+//Se relaciona con id de ORDER e id de PRODUCT
 module.exports = (sequelize) => {
-  sequelize.define('order_detail', {
-    quantity: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    price: {
-      type: DataTypes.FLOAT,
-      allowNull:false
-    },
-  },
-    {
-      timestamps: false
-    }
-  );
+	sequelize.define(
+		'order_detail',
+		{
+            // quantity es para saber cuantos productos se compro
+			quantity: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+			},
+            // price es para saber cuanto costo el producto
+			price: {
+				type: DataTypes.FLOAT,
+				allowNull: false,
+			},
+		},
+		{
+			timestamps: false,
+		}
+	);
 };
 
-//Se relaciona con id de ORDER y PRODUCT
