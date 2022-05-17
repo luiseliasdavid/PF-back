eb8cbae635e1b4cb32c117a4446dff5eea0a4db1
Darwin Esteban Cifuentes G <tecnicoesteban01@gmail.com>
2022-05-17 01:23:07 -0500

    fixed port

--- api/src/db.js
+++ api/src/db.js
@@ -10,9 +10,10 @@ const db={
   host: DB_HOST || "ec2-52-71-69-66.compute-1.amazonaws.com",
   name: DB_NAME || "d8rp7epoiokuee",
   password: DB_PASSWORD || 'f913d8ff3aa50c39a434e8b805200e15c059c06436baa8aaa85551a41fe03cc2'
+
 }
 
-const sequelize = new Sequelize(`postgres://${db.user}:${db.password}@${db.host}/${db.name}`, {
+const sequelize = new Sequelize(`postgres://${db.user}:${db.password}@${db.host}:5432/${db.name}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
