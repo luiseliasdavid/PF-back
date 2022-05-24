844a99060248f063f5d73e413b1f6e401208a8ea
Darwin Esteban Cifuentes G <tecnicoesteban01@gmail.com>
2022-05-23 19:21:50 -0500

    rutas nuevas admin

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -16,6 +16,13 @@ const deleteCategory = require("./functions/deleteCategory.js");
 const createModel = require("./functions/createModel.js");
 const createSneaker = require('./functions/createSneaker');
 const payment = require("./functions/payment.js");
+const getModels = require('./functions/getModels')
+const getColors = require('./functions/getColors')
+const getMaterials = require('./functions/getModels')
+const getSizes = require('./functions/getSizes')
+
+
+
 
 const router = Router();
 
