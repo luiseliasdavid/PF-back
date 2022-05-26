a78235d2feab147025e4571ccd89a3a13a4ae043
Darwin Esteban Cifuentes G <tecnicoesteban01@gmail.com>
2022-05-26 04:42:21 -0500

    update sneaekr cool

--- api/src/routes/functions/updateSneaker.js
+++ api/src/routes/functions/updateSneaker.js
@@ -1,4 +1,5 @@
-const { Sneaker } = require("../../db")
+
+const { Sneaker, Model, Size } = require("../../db")
 
 
 const updateSneaker = async (req, res) => {
@@ -7,14 +8,28 @@ const updateSneaker = async (req, res) => {
   console.log(props, 'aquiiiii')
   try {
     if (id) {
-      const sneaker = await Sneaker.findByPk(id)
+      const sneaker = await Sneaker.findByPk(id,{
+        attributes: { exclude: ['colorId', 'modelId'] },
+        include: { all: true, nested: true }})
       if (!sneaker) {
         res.status(404).send({ msg: "The sneaker does not exist" });
       }
 
-      const newData = await sneaker.update(props)
-      console.log(newData)
-      if (newData) {
+      // const newData = await sneaker.update({price: props.price})
+      sneaker.price = props.price;
+      await sneaker.save()
+
+      const model = await Model.findByPk(1, {include: { all: true, nested: true }})
+
+      props.sizes.forEach( n => {
+        model.sizes.map(async(s, i) => {
+          if(s.numberSize === n.numberSize){
+            await model.sizes[i].modelsize.update({stock: n.stock})
+          }
+        })
+      });
+      
+      if (sneaker) {
         res.json({
           msg: "The sneaker has been updated",
         });
